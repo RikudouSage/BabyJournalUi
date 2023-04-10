b513479992894622d2ba17120e0cdcccaa2bab14
Dominik Chrastecky <dominik@chrastecky.cz>
2023-04-10 13:26:40 +0200

    Add route hierarchy

---
+++ src/app/helper/route-hierarchy.ts
@@ -0,0 +1,52 @@
+interface RouteHierarchy {
+  [path: string]: RouteHierarchy;
+}
+
+interface CachedRouteHierarchy {
+  [path: string]: string;
+}
+
+const routeHierarchy: RouteHierarchy = {
+  '': {
+    'settings': {
+      'settings/account': {
+        'settings/account/export': {},
+        'settings/account/sharing': {
+          'settings/account/sharing/invite': {},
+        },
+      },
+    },
+    'children/select-child': {
+      'children/add': {},
+    },
+    'auth/logout': {},
+  },
+  'auth/register': {},
+  'children/create-first': {},
+  'unsupported-browser': {},
+};
+
+let cachedRouteHierarchy: CachedRouteHierarchy | null = null;
+
+function compileRouteHierarchy(hierarchy: RouteHierarchy, parent: string = ''): void {
+  if (cachedRouteHierarchy === null) {
+    cachedRouteHierarchy = {};
+  }
+  for (const routeName of Object.keys(hierarchy)) {
+    const route = hierarchy[routeName];
+    cachedRouteHierarchy[routeName] = parent;
+    compileRouteHierarchy(route, routeName);
+  }
+}
+
+export function findRouteParent(route: string): string {
+  if (cachedRouteHierarchy === null) {
+    compileRouteHierarchy(routeHierarchy);
+  }
+  const routes = <CachedRouteHierarchy>cachedRouteHierarchy;
+  if (typeof routes[route] === 'undefined') {
+    throw new Error(`Failed to find parent for route: '${route}'`);
+  }
+
+  return routes[route];
+}
