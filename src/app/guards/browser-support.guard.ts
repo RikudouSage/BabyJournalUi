9ba30aaf06651443167fa51decffc0c941aa0703
Dominik Chrastecky <dominik@chrastecky.cz>
2023-10-08 12:42:56 +0200

    Update to Angular 16

--- src/app/guards/browser-support.guard.ts
+++ src/app/guards/browser-support.guard.ts
@@ -1,47 +1,18 @@
-import {Injectable} from "@angular/core";
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  CanActivateChild,
-  Router,
-  RouterStateSnapshot,
-  UrlTree
-} from "@angular/router";
+import {inject} from "@angular/core";
+import {CanActivateFn, Router, UrlTree} from "@angular/router";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class BrowserSupportGuard implements CanActivate, CanActivateChild {
+export const BrowserSupportGuard: CanActivateFn = (): true | UrlTree => {
+  const features = [
+    window.crypto,
+    window.crypto.subtle,
+    navigator.clipboard,
+  ];
 
-  constructor(
-    private readonly router: Router,
-  ) {
-  }
-
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): boolean | UrlTree {
-    const features = [
-      window.crypto,
-      window.crypto.subtle,
-      navigator.clipboard,
-    ];
-
-    for (const feature of features) {
-      if (typeof feature === 'undefined') {
-        return this.router.createUrlTree(['/unsupported-browser']);
-      }
+  for (const feature of features) {
+    if (typeof feature === 'undefined') {
+      return inject(Router).createUrlTree(['/unsupported-browser']);
     }
-
-    return true;
   }
 
-  canActivateChild(
-    childRoute: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): boolean | UrlTree {
-    return this.canActivate(childRoute, state);
-  }
-
+  return true;
 }
--- src/app/guards/has-children.guard.ts
+++ src/app/guards/has-children.guard.ts
@@ -1,46 +1,17 @@
-import {Injectable} from "@angular/core";
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  CanActivateChild,
-  Router,
-  RouterStateSnapshot,
-  UrlTree
-} from "@angular/router";
+import {inject} from "@angular/core";
+import {CanActivateFn, Router, UrlTree} from "@angular/router";
 import {ChildRepository} from "../entity/child.entity";
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class HasChildrenGuard implements CanActivate, CanActivateChild {
-  constructor(
-    private readonly childRepository: ChildRepository,
-    private readonly router: Router,
-  ) {
-  }
+export const HasChildrenGuard: CanActivateFn = (): Observable<true | UrlTree> => {
+  return inject(ChildRepository).collection().pipe(
+    map(result => {
+      if (result.totalItems < 1) {
+        return inject(Router).createUrlTree(['/children/create-first']);
+      }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> {
-    return this.childRepository.collection().pipe(
-      map(result => {
-        if (result.totalItems < 1) {
-          return this.router.createUrlTree(['/children/create-first']);
-        }
-
-        return true;
-      }),
-    );
-  }
-
-  canActivateChild(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> {
-    return this.canActivate(route, state);
-  }
-
+      return true;
+    }),
+  );
 }
