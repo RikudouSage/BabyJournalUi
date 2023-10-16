c07180beef07591c94f1fdb0cff6bd9230796134
Dominik Chrastecky <dominik@chrastecky.cz>
2023-10-16 19:35:14 +0200

    Switch back to class-based guard

--- src/app/guards/initial-load-finished.guard.ts
+++ src/app/guards/initial-load-finished.guard.ts
@@ -1,11 +1,41 @@
-import {inject} from '@angular/core';
-import {CanActivateFn, Router, UrlTree} from '@angular/router';
+import {Injectable} from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from '@angular/router';
 import {DatabaseService} from "../services/database.service";
 
-export const InitialLoadFinishedGuard: CanActivateFn = async (): Promise<true | UrlTree> => {
-  if (!await inject(DatabaseService).initialActivityStreamLoadFinished()) {
-    return inject(Router).createUrlTree(['/full-data-refresh']);
+@Injectable({
+  providedIn: 'root'
+})
+export class InitialLoadFinishedGuard implements CanActivate, CanActivateChild {
+
+  constructor(
+    private readonly router: Router,
+    private readonly database: DatabaseService,
+  ) {
   }
 
-  return true;
+  async canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Promise<boolean | UrlTree> {
+    if (!await this.database.initialActivityStreamLoadFinished()) {
+      return this.router.createUrlTree(['/full-data-refresh']);
+    }
+
+    return true;
+  }
+
+  async canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Promise<boolean | UrlTree> {
+    return await this.canActivate(route, state);
+  }
+
 }
